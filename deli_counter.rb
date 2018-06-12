0c2542295581d48393a0ef5acd1eafac503163bc
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 12:55:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  line_method_array = []
+  if line == 0
+    puts "The line is currently empty."
+  else
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push ("#{name}. #{index}")
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
+  end
+
+end
+
+
+#if nobody is in line: "The line is currently empty."
+
+
+
