d1ffe6d0d05575ccff3434d39f4bbcaa5bf50143
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 12:56:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 
 katz_deli = []
-
+#line
 def line(katz_deli)
   phrase = "The line is currently: "
-  if katz_deli.length > 0
-    katz_deli.each_with_index do |name, index| phrase
-			phrase += "#{index + 1}. #{name} "
-		end
-		puts phrase
+  if katz_deli.length > 0 
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
   else
-   puts "The line is currently enpty."
+    puts "The line is currently empty."
   end
 end
 
