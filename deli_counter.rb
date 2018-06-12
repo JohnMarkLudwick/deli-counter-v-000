d97d0990ec0b7e425a4cf2ec947ce4597ea53e72
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 12:57:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(katz_deli)
   phrase = "The line is currently: "
   if katz_deli.length > 0
-    katz_deli.each_with_index do |name, index| phrase
+    katz_deli.each_with_index do |name, index|
 			phrase += "#{index + 1}. #{name} "
 		end
 		puts phrase
@@ -13,7 +13,19 @@ def line(katz_deli)
   end
 end
 
-
+katz_deli = []
+#line
+def line(katz_deli)
+  phrase = "The line is currently: "
+  if katz_deli.length > 0 
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
+  end
+end
 
 
 
