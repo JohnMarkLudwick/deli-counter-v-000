a894d45b4c2a1665b8c260c426ff8ad3853ddc8c
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 15:10:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,20 @@
 
 
-katz_deli = ["Moshe", "Fayge", "Rivki"]
+katz_deli = []
 
-
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
+def line(katz_deli)
+  phrase = "The line is currently: "
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
   else
     puts "The line is currently empty."
   end
 end
+
+
 
 
 
