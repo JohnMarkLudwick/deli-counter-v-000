22e43b24d3c532b2220db72e0c6c06f44cfff7dd
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 12:56:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,43 @@
+
 katz_deli = []
 
 def line(katz_deli)
-  line_method_array = []
-  if line == 0
-    puts "The line is currently empty."
+  phrase = "The line is currently: "
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, index| phrase
+			phrase += "#{index + 1}. #{name} "
+		end
+		puts phrase
   else
-    line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push ("#{name}. #{index}")
-    end
-    puts "The line is: #{line_method_array.join(" ")}"
+   puts "The line is currently enpty."
   end
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# katz_deli = []
+
+# def line(katz_deli)
+#   line_method_array = []
+#   if line == 0
+#     puts "The line is currently empty."
+#   else
+#     line_method_array.each.with_index(0) do |name, index|
+#       line_method_array.push ("#{name}. #{index}")
+#     end
+#     puts "The line is: #{line_method_array.join(" ")}"
+#   end
+
+# end
 
 
 #if nobody is in line: "The line is currently empty."
