e874ae8e97fed2ac0e9473064c493479450bd9d5
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 14:54:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,54 +1,74 @@
-katz_deli = ["Moshe", "Fayge", "Rivki"]
 
+katz_deli = []
 
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
+def line(katz_deli)
+  phrase = "The line is currently: "
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+		end
+		puts phrase
   else
-    puts "The line is currently empty."
+  puts "The line is currently enpty."
   end
 end
 
-line(katz_deli)
 
-def line_simple(array) # this one follows the same mechanics as learn.co
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"    
-    # "indexemus" is used to illustrate this variable can be different from "index"
-  end 
-  puts current_line
-end 
-  
-line_simple(katz_deli)  
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
+# katz_deli = []
+
+# def line(katz_deli)
+#   phrase = "The line is currently: "
+#   if katz_deli.length > 0
+#     katz_deli.each_with_index do |name, index|
+#       phrase += "#{index + 1}. #{name} "
+#     end
+#     puts phrase
+#   else
+#     puts "The line is currently empty."
+#   end
+# end
 
 
+# katz_deli = []
+# #line
+# def line(katz_deli)
+#   phrase = "The line is currently: "
+#   if katz_deli.length > 0 
+#     katz_deli.each_with_index do |name, index|
+#       phrase += "#{index + 1}. #{name} "
+#     end
+#     puts phrase
+#   else
+#     puts "The line is currently empty."
+#   end
+# end
 
-def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
 
-take_a_number(katz_deli, "Fyvish")
 
-def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
-    puts"There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
-  end
-end
 
-puts now_serving(katz_deli)
-puts katz_deli
+
+
+
+
+
+
+# katz_deli = []
+
+# def line(katz_deli)
+#   line_method_array = []
+#   if line == 0
+#     puts "The line is currently empty."
+#   else
+#     line_method_array.each.with_index(0) do |name, index|
+#       line_method_array.push ("#{name}. #{index}")
+#     end
+#     puts "The line is: #{line_method_array.join(" ")}"
+#   end
+
+# end
+
+
+#if nobody is in line: "The line is currently empty."
+
+
+
