634de0cbb67127560c286e8618ba8eee61c9eeb0
JohnMark <johnmarkludwick@gmail.com>
2018-06-12 13:06:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,22 @@
 #   end
 # end
 
+
+katz_deli = []
+
+def line(katz_deli)
+  phrase = "The line is currently: "
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
+  end
+end
+
+
 katz_deli = []
 #line
 def line(katz_deli)
